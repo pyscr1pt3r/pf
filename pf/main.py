f3e1748794940faa082b14794a499b69648aab6c
coder <pyscr1pt3r@proton.me>
2024-02-06 16:41:45 +0800

    fix

--- pf/main.py
+++ pf/main.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-import os, sys
+import os
+import sys
 import argparse
-import logging
-import time
-import colorama
-from colorama import Fore, Style
 from urllib.parse import urlparse, parse_qs, urlencode
-from .core.http import fetch
 
 
-HARDCODED_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".pdf", ".svg", ".json", ".css", ".js", ".webp", ".woff", ".woff2", ".eot", ".ttf", ".otf", ".mp4", ".txt"]
-
-yellow_color_code = "\033[93m"
-reset_color_code = "\033[0m"
-
-colorama.init(autoreset=True)  # Initialize colorama for colored terminal output
-
-logging.basicConfig(format='%(message)s', level=logging.INFO, stream=sys.stderr, datefmt='%Y-%m-%d %H:%M:%S')
+extensions = [".jpg", ".jpeg", ".png", ".gif", ".pdf", ".svg", ".json", ".css", ".js", ".webp", ".woff", ".woff2", ".eot", ".ttf", ".otf", ".mp4", ".txt"]
+j_params = ['utm_campaign']
 
 p = argparse.ArgumentParser(description="Replace URL values with keyword")
 p.add_argument("-p", "--placeholder", help="placeholder for parameter values", default="FUZZ")
@@ -24,8 +14,8 @@ args = p.parse_args()
 
 
 def has_extension(url, extensions):
-    parsed_url = urlparse(url)
-    path = parsed_url.path
+    parsed = urlparse(url)
+    path = parsed.path
     extension = os.path.splitext(path)[1].lower()
 
     return extension in extensions
@@ -44,11 +34,15 @@ def clean_url(url):
     return parsed.geturl()
 
 
+def remove_junk_params(url):
+
+    ...
+
+
 def main():
     if sys.stdin.isatty():
         sys.exit(0)
 
-    extensions = HARDCODED_EXTENSIONS
     urls = set()
     for line in sys.stdin.readlines():
         url = line.strip('\r').strip('\n')
@@ -56,11 +50,11 @@ def main():
             continue
         if has_extension(url, extensions):
             continue
-        parsed_url = urlparse(url)
-        query_params = parse_qs(parsed_url.query)
+        parsed = urlparse(url)
+        query_params = parse_qs(parsed.query)
         cleaned_params = {key: args.placeholder for key in query_params}
         cleaned_query = urlencode(cleaned_params, doseq=True)
-        url = parsed_url._replace(query=cleaned_query).geturl()
+        url = parsed._replace(query=cleaned_query).geturl()
         urls.add(url)
 
     for url in urls:
